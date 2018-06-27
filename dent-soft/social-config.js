c35ec7d9274999b196100414855ea2017486e33a
Mojib Mohammad <mojib_monir@yahoo.com>
2018-06-27 12:31:14 -0700

    added incomplete social login files

---
+++ dent-soft/social-config.js
@@ -0,0 +1,23 @@
+const facebook = {
+    appId: '609626619396590',
+    cookie: true,
+    xfbml: true,
+    version: 'v2.11',
+    scope: 'email'
+  };
+   
+  export const facebookConfig = facebook;
+
+  const google = {
+        "installed":{"client_id":"262477568106-m6lhrti9r9its0an1tegib7sf6rm3de4.apps.googleusercontent.com",
+        "project_id":"dent-soft-1530117228535",
+        "auth_uri":"https://accounts.google.com/o/oauth2/auth",
+        "token_uri":"https://accounts.google.com/o/oauth2/token",
+        "auth_provider_x509_cert_url":"https://www.googleapis.com/oauth2/v1/certs",
+        "client_secret":"zdOkAYZrqrBTK9CSG1DOr_oK",
+        "redirect_uris":["urn:ietf:wg:oauth:2.0:oob","http://localhost"
+  ]}};
+      
+  
+   
+  export const googleConfig = google;
---
+++ dent-soft/src/components/FormWrap.js
@@ -0,0 +1,225 @@
+/**
+ * Created by meller.olaf@gmail.com on 11/22/2017.
+ */
+
+import React from "react";
+
+import FormSubmitButton from "./FormSubmitButton";
+
+
+export default class FormWrap extends React.Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  componentWillReceiveProps(nextProps) {
+
+    if (!this.props.visible && nextProps.visible ) {
+      for (const ref in this.refs) {
+        this.refs[ref].value = "";
+      }
+    }
+  }
+
+  /**
+   *
+   * @constructor
+   */
+  render() {
+
+    const formProps = this.props.form;
+
+    const loginBtn = formProps && formProps.loginBtn
+      ? <FormSubmitButton
+          buttonClass={
+            formProps.loginBtn.buttonClass
+              ? formProps.loginBtn.buttonClass
+              : "RML-btn"
+          }
+          type="login"
+          inactive={this.props.inactive}
+          click={formProps.onLogin ? formProps.onLogin : null}
+          label={formProps.loginBtn.label ? formProps.loginBtn.label : "Sign in"}
+        />
+      : null;
+
+    const registerBtn = formProps && formProps.registerBtn
+      ? <FormSubmitButton
+          buttonClass={
+            formProps.registerBtn.buttonClass
+              ? formProps.registerBtn.buttonClass
+              : "RML-btn"
+          }
+          type="register"
+          inactive={this.props.inactive}
+          click={formProps.onRegister ? formProps.onRegister : null}
+          label={formProps.registerBtn.label ? formProps.registerBtn.label : "Sign up"}
+        />
+      : null;
+
+    const recoverPasswordBtn = formProps && formProps.recoverPasswordBtn
+      ? <FormSubmitButton
+          buttonClass={
+            formProps.recoverPasswordBtn.buttonClass
+              ? formProps.recoverPasswordBtn.buttonClass
+              : "RML-btn"
+          }
+          type="recoverPassword"
+          inactive={this.props.inactive}
+          click={formProps.onRecoverPassword ? formProps.onRecoverPassword : null}
+          label={formProps.recoverPasswordBtn.label ? formProps.recoverPasswordBtn.label : "Recover password"}
+        />
+      : null;
+
+    const recoverPasswordAnchor = formProps && formProps.recoverPasswordAnchor
+      ? <span
+          className={
+            formProps.recoverPasswordAnchor.anchorClass
+              ? formProps.recoverPasswordAnchor.anchorClass
+              : "RML-recover-password-anchor"
+          }
+          onClick={() => {
+            this.props.recoverPasswordAnchorClick();
+          }}
+        >
+          {formProps.recoverPasswordAnchor.label}
+        </span>
+      : null;
+
+    const formLoginInputs = formProps.loginInputs
+      ? formProps.loginInputs.map((input, index) => {
+        return (
+          <div
+            className={input.containerClass ? input.containerClass : "RML-form-group"}
+            key={index}
+          >
+            <label htmlFor={input.id}>{input.label}</label>
+            <input
+              type={input.type}
+              className={input.inputClass ? input.inputClass : "RML-form-control"}
+              id={input.id}
+              name={input.name}
+              ref={"formLoginInput-" + index}
+              key={"formLoginInput-" + index}
+              defaultValue=""
+              placeholder={input.placeholder}
+            />
+          </div>
+        )
+      })
+      : null;
+
+    const formRegisterInputs = formProps.registerInputs
+      ? formProps.registerInputs.map((input, index) => {
+        return (
+          <div
+            className={input.containerClass ? input.containerClass : "RML-form-group"}
+            key={index}
+          >
+            <label htmlFor={input.id}>{input.label}</label>
+            <input
+              type={input.type}
+              className={input.inputClass ? input.inputClass : "RML-form-control"}
+              id={input.id}
+              name={input.name}
+              ref={"formRegisterInput-" + index}
+              key={"formRegisterInput-" + index}
+              defaultValue=""
+              placeholder={input.placeholder}
+            />
+          </div>
+        )
+      })
+      : null;
+
+    const formRecoverPasswordInputs = formProps.recoverPasswordInputs
+      ? formProps.recoverPasswordInputs.map((input, index) => {
+          return (
+            <div
+              className={input.containerClass ? input.containerClass : "RML-form-group"}
+              key={index}
+            >
+              <label htmlFor={input.id}>{input.label}</label>
+              <input
+                type={input.type}
+                className={input.inputClass ? input.inputClass : "RML-form-control"}
+                id={input.id}
+                name={input.name}
+                ref={"formRecoverPasswordInput-" + index}
+                key={"formRecoverPasswordInput-" + index}
+                defaultValue=""
+                placeholder={input.placeholder}
+              />
+            </div>
+          )
+        })
+      : null;
+
+    const recoverPasswordSuccessLabel = this.props.recoverPasswordSuccessLabel
+      ? <span
+          className={
+            this.props.recoverPasswordSuccessLabel.labelClass
+              ? this.props.recoverPasswordSuccessLabel.labelClass
+              : "RML-recover-password-success-label"
+          }
+          id="recoverPasswordSuccessLabel"
+        >
+          {this.props.recoverPasswordSuccessLabel.label}
+        </span>
+      : null;
+
+    let formWrap = null;
+
+    switch (this.props.currentTab) {
+      case "register":
+        formWrap = (
+          <div className={formProps.registerContainerClass ? formProps.registerContainerClass : "RML-login-modal-form"}>
+
+            {formRegisterInputs}
+            {formProps.bottomRegisterContainer}
+            {this.props.errorWrap}
+            {registerBtn}
+            {this.props.loader}
+
+            <div className="clearfix" />
+          </div>
+        );
+      break;
+
+      case "login":
+        formWrap = (
+          <div className={formProps.loginContainerClass ? formProps.loginContainerClass : "RML-login-modal-form"}>
+
+            {formLoginInputs}
+            {formProps.bottomLoginContainer}
+            {this.props.errorWrap}
+            {recoverPasswordAnchor}
+            {loginBtn}
+            {this.props.loader}
+
+            <div className="clearfix" />
+          </div>
+        );
+      break;
+
+      case "recoverPassword":
+        formWrap = (
+          <div className={formProps.recoverPasswordContainerClass ? formProps.recoverPasswordContainerClass : "RML-login-modal-form"}>
+
+            {formRecoverPasswordInputs}
+            {formProps.bottomRecoverPasswordContainer}
+            {recoverPasswordSuccessLabel}
+            {this.props.errorWrap}
+            {recoverPasswordBtn}
+            {this.props.loader}
+
+            <div className="clearfix" />
+          </div>
+        );
+        break;
+    }
+
+    return formWrap;
+  }
+};
