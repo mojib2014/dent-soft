1e87fc65f6b487a2f00cb9aa0954a6084a383d66
Mojib Mohammad <mojib_monir@yahoo.com>
2018-06-29 13:58:24 -0700

    add contact form

---
+++ client/src/pages/Main/main.js
@@ -0,0 +1,35 @@
+import React, {Component} from "react";
+import Login from "../../components/Login";
+import ContactForm from "../../components/ContactForm";
+import { Col, Row, Container } from "reactstrap";
+
+
+
+class Main extends Component {
+    state = {
+        username:"",
+        password:""
+    };
+
+    render() {
+        return (
+            <Container fluid> 
+                    <Row>
+                       <Col size="md-6">
+                            <div className="intro">
+                                <p>Great William is Watching You!</p>
+                            </div>
+                       </Col>
+                       <Col size="md-6">
+                            <div className="Login">
+                                <Login />
+                            </div>
+                       </Col> 
+                    </Row>
+                    <ContactForm />
+            </Container>
+        );
+    }
+}
+
+export default Main;
