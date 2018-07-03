fcb7d7ceb30c20ae4df53c08164956c0cefd0619
Marlon <marlon_j_J@yahoo.com>
2018-07-03 13:29:22 -0700

    Profile Information Editing functional

--- client/src/App.js
+++ client/src/App.js
@@ -5,26 +5,27 @@ import NoMatch from "./pages/NoMatch";
 import Dentist from "./pages/Dentist";
 import Patients from "./pages/Patients";
 import Footer from "./components/Footer";
-
+import Nav from "./components/Nav";
 
 class App extends Component {
 
   render() {
     return (
+      <div>
+      <Nav >Dental Connect</Nav>
       <Router>
-        <div>
           <Switch>
             <Route exact path="/" component={Main} />
             <Route exact path="/dentist" component={Dentist} />
-            <Route exact path="/patient/:id" component={Patients} />
+            <Route exact path="/patient" component={Patients} />
             <Route exact path="/auth/login" component={Main} />
             {/* <Route exact path="/auth/google" component={Main} /> */}
             <Route exact path="/auth/logout" component={Main} />
             <Route component={NoMatch} />
           </Switch>
-          <Footer />
-        </div>
       </Router>
+      <Footer />
+      </div>
     )
   }
 };
